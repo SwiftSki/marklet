87f7ffbc501f9576fa174287e6487fa6aa1f2267
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-20 18:43:29 -0600

    Create bash.js

---
+++ bash.js
@@ -0,0 +1,13 @@
+function feat(){
+  switch(prompt('service\ntype "help" for help')){
+    case 'help':
+    break;
+    case 'flash':
+    break;
+    case 'tool':
+    break;
+
+    default:
+    break;
+  }
+}
