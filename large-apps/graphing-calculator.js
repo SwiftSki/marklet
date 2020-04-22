b8470938d652e6b6d18cf99f840280b5c3a0dc51
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-22 09:35:12 -0600

    Create graphing-calculator.js

---
+++ large-apps/graphing-calculator.js
@@ -0,0 +1,10 @@
+var hi = document.createElement('iframe');
+hi.src = "https://www.desmos.com/calculator";
+hi.style.background = 'blue';
+hi.style.position = 'fixed';
+hi.style.width = '50%';
+hi.style.height = '50%';
+hi.style.top = '40%';
+hi.style.left = '50%';
+document.body.appendChild(hi);
+
