cbdc56d3386dd4e3d02e83ca770741196e32ff11
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2020-04-22 09:34:24 -0600

    Create calculator.js

---
+++ large-apps/calculator.js
@@ -0,0 +1,10 @@
+var hi = document.createElement('iframe');
+hi.src = "https://www.desmos.com/scientific";
+hi.style.background = 'blue';
+hi.style.position = 'fixed';
+hi.style.width = '30%';
+hi.style.height = '50%';
+hi.style.top = '40%';
+hi.style.left = '70%'; hi.id = 'hi';
+document.body.appendChild(hi);
+
